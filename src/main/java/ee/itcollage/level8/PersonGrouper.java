f14480c3a8db2a34ff3f0163d92706b202e2d827
mhasan <mhasan@taltech.ee>
2019-03-07 13:47:49 +0200

    PersonGrouper test works

--- src/main/java/ee/itcollage/level8/PersonGrouper.java
+++ src/main/java/ee/itcollage/level8/PersonGrouper.java
@@ -8,7 +8,15 @@ public class PersonGrouper {
 
     //todo fix tests
     public static Map<String, Person> groupPeopleByIdCode(List<Person> people){
-        return new HashMap<>();
+        HashMap<String,Person> h_map = new HashMap<String, Person>(); //Creates a New Hashmap call h_map
+                                                                    // The key is the String value of a person ID, value is the person obj itself.
+        for(Person obj : people) //gets every (people)object from the people List
+        {
+            h_map.put( String.valueOf( obj.getIdCode()) , obj ); //Inserts object in to the list, Note that the key is the String value of the the int Id code of a person object.
+        }
+
+        return h_map;
+
     }
 
 }
